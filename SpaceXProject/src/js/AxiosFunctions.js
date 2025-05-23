f0ba2928e98c5f48203413f8ab8c81ed97d16bad
borbely-dominik-peter <98750586+borbely-dominik-peter@users.noreply.github.com>
2025-05-23 10:23:45 +0000

    more data for decoding
    version of api changed to v4, v5 doesn't have all data

--- SpaceXProject/src/js/AxiosFunctions.js
+++ SpaceXProject/src/js/AxiosFunctions.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import Rocket from "./Rocket";
 
 const Axios = axios.create({
-    baseURL: "https://api.spacexdata.com/v5/",
+    baseURL: "https://api.spacexdata.com/v4/",
     headers: {
         "Content-Type": "application/json"
     }
@@ -11,6 +11,38 @@ const Axios = axios.create({
 export default{
     async GetAllData(){
         return Axios.get("/launches")
+            .then((res) => {
+                return res.data;
+            }).catch((err) => {
+                console.log(err);
+            });
+    },
+    async GetAllRockets(){
+        return Axios.get("/rockets")
+            .then((res) => {
+                return res.data;
+            }).catch((err) => {
+                console.log(err);
+            });
+    },
+    async GetAllCrew(){
+        return Axios.get("/crew")
+            .then((res) => {
+                return res.data;
+            }).catch((err) => {
+                console.log(err);
+            });
+    },
+    async GetAllCapsules(){
+        return Axios.get("/capsules")
+            .then((res) => {
+                return res.data;
+            }).catch((err) => {
+                console.log(err);
+            });
+    },
+    async GetAllPayloads(){
+        return Axios.get("/payloads")
             .then((res) => {
                 return res.data;
             }).catch((err) => {
