ebad33b0a0848b61b280b71ccad5cb9f4d44a1e5
borbely-dominik-peter <98750586+borbely-dominik-peter@users.noreply.github.com>
2025-05-26 16:08:19 +0000

    Details managing
    3 selects to list out data about 1 crew/capsule/payload
    styling needed

--- SpaceXProject/src/js/RoutingClasses/details.js
+++ SpaceXProject/src/js/RoutingClasses/details.js
@@ -3,7 +3,53 @@ import AxiosFunctions from "../AxiosFunctions.js";
 export default class Details{
     constructor() {
         this.FillSelects();
-        
+        this.DataTarget = document.querySelector("#DataTarget");
+        document.querySelector("#CrewSelect").addEventListener("change", async (event) => {
+            if (event.target.value == "null") {
+                this.NullOptionHandler();
+                return;
+            }
+            document.querySelector("#CapsuleSelect").style.visibility = "hidden";
+            document.querySelector("#PayloadSelect").style.visibility = "hidden";
+            const SelectedIdData = await AxiosFunctions.GetAllDataByID("/crew",event.target.value)
+            this.DataTarget.innerHTML = `
+                <img src='${SelectedIdData.image}' alt='...'>
+                <h1>${SelectedIdData.name}</h1>
+                <p>Employed by: <b>${SelectedIdData.agency}</b></p>
+                <p>Current Status: <b>${SelectedIdData.status}</b></p>
+            `;
+        });
+        document.querySelector("#CapsuleSelect").addEventListener("change", async (event) => {
+            if (event.target.value == "null") {
+                this.NullOptionHandler()
+                return;
+            }
+            document.querySelector("#CrewSelect").style.visibility = "hidden";
+            document.querySelector("#PayloadSelect").style.visibility = "hidden";
+            const SelectedIdData = await AxiosFunctions.GetAllDataByID("/capsules",event.target.value)
+            this.DataTarget.innerHTML = `
+                <h1>${SelectedIdData.type}(<b>${SelectedIdData.serial}</b>)</h1>
+                <p><b>Status:</b>${SelectedIdData.status}</p>
+                <p><b>Last Update:</b>${ SelectedIdData.last_update != null ? SelectedIdData.last_update : "No Information available"}</p>
+            `;
+        });
+        document.querySelector("#PayloadSelect").addEventListener("change", async (event) => {
+            if (event.target.value == "null") {
+                this.NullOptionHandler();
+                return;
+            }
+            document.querySelector("#CapsuleSelect").style.visibility = "hidden";
+            document.querySelector("#CrewSelect").style.visibility = "hidden";
+            const SelectedIdData = await AxiosFunctions.GetAllDataByID("/payloads",event.target.value)
+            let ManufacturerConcat = SelectedIdData.manufacturers.join(", ");
+            let CustomerConcat = SelectedIdData.customers.join(", ");
+            this.DataTarget.innerHTML = `
+                <h1>${SelectedIdData.name}</h1>
+                <p><b>Type:</b> ${SelectedIdData.type}</p>
+                <p><b>Manufacturer(s): </b>${ManufacturerConcat}</p>
+                <p><b>Customer(s): </b>${CustomerConcat}</p>
+            `; 
+        });
     }
 
     FillSelects = async () => {
@@ -19,17 +65,33 @@ export default class Details{
             opt.innerHTML = Crew.name;
             CrewSel.appendChild(opt);
         });
-        CapsuleData.forEach(Crew => {
+        CapsuleData.forEach(Capsule => {
             let opt = document.createElement("option");
-            opt.value = Crew.id;
-            opt.innerHTML = Crew.type;
+            opt.value = Capsule.id;
+            opt.innerHTML = `${Capsule.type}(<b>${Capsule.serial}</b>)`;
             CapsuleSel.appendChild(opt);
         });
-        PayloadData.forEach(Crew => {
+        PayloadData.forEach(Payload => {
             let opt = document.createElement("option");
-            opt.value = Crew.id;
-            opt.innerHTML = Crew.name;
+            opt.value = Payload.id;
+            opt.innerHTML = Payload.name;
             PayloadSel.appendChild(opt);
         });
     }
+
+    VisReset()
+    {
+        document.querySelector("#CapsuleSelect").style.visibility = "visible";
+        document.querySelector("#PayloadSelect").style.visibility = "visible";
+        document.querySelector("#CrewSelect").style.visibility = "visible";
+    }
+
+    NullOptionHandler(){
+        this.DataTarget.innerHTML = "";
+        this.VisReset();
+    }
+
+
+
+
 }
