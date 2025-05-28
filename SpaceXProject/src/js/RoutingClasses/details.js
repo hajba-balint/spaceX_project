1693965b805c2df8ee5dd618bc5e5fab14a34d32
hajba-balint <hajba.balint@students.jedlik.eu>
2025-05-28 18:14:06 +0200

    details all cards

--- SpaceXProject/src/js/RoutingClasses/details.js
+++ SpaceXProject/src/js/RoutingClasses/details.js
@@ -2,6 +2,7 @@ import AxiosFunctions from "../AxiosFunctions.js";
 
 export default class Details{
     constructor() {
+        this.HideTopButton();
         this.FillSelects();
         this.DataTarget = document.querySelector("#DataTarget");
         document.querySelector("#CrewSelect").addEventListener("change", async (event) => {
@@ -34,9 +35,15 @@ export default class Details{
             document.querySelector("#PayloadSelect").style.visibility = "hidden";
             const SelectedIdData = await AxiosFunctions.GetAllDataByID("/capsules",event.target.value)
             this.DataTarget.innerHTML = `
-                <h1>${SelectedIdData.type}(<b>${SelectedIdData.serial}</b>)</h1>
-                <p><b>Status:</b>${SelectedIdData.status}</p>
-                <p><b>Last Update:</b>${ SelectedIdData.last_update != null ? SelectedIdData.last_update : "No Information available"}</p>
+            <div class='col-lg-3 col-md-6 col-sm-12 m-auto'>
+                <div class='card'>
+                    <div class='card-body'>
+                        <h1>${SelectedIdData.type}(<b>${SelectedIdData.serial}</b>)</h1>
+                        <p><b>Status: </b>${SelectedIdData.status}</p>
+                        <p><b>Last Update: </b>${ SelectedIdData.last_update != null ? SelectedIdData.last_update : "No Information available"}</p>
+                    </div>
+                </div>
+            </div>
             `;
         });
         document.querySelector("#PayloadSelect").addEventListener("change", async (event) => {
@@ -50,10 +57,16 @@ export default class Details{
             let ManufacturerConcat = SelectedIdData.manufacturers.join(", ");
             let CustomerConcat = SelectedIdData.customers.join(", ");
             this.DataTarget.innerHTML = `
-                <h1>${SelectedIdData.name}</h1>
-                <p><b>Type:</b> ${SelectedIdData.type}</p>
-                <p><b>Manufacturer(s): </b>${ManufacturerConcat}</p>
-                <p><b>Customer(s): </b>${CustomerConcat}</p>
+            <div class='col-lg-3 col-md-6 col-sm-12 m-auto'>
+                <div class='card'>
+                    <div class='card-body'>
+                        <h1>${SelectedIdData.name}</h1>
+                        <p><b>Type:</b> ${SelectedIdData.type}</p>
+                        <p><b>Manufacturer(s): </b>${ManufacturerConcat}</p>
+                        <p><b>Customer(s): </b>${CustomerConcat}</p>
+                    </div>
+                </div>
+            </div>
             `; 
         });
     }
@@ -97,7 +110,9 @@ export default class Details{
         this.VisReset();
     }
 
-
+    HideTopButton(){
+        document.querySelector("#topButton").style.display = "none";
+    }
 
 
 }
